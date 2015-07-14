f90621c27891d59369bc09741ad2ae2ddb4033fb
Jason Sayre <ejsayre@gmail.com>
2015-07-14 10:10:52 -0400

    Added scheme for gnome-terminal

---
+++ gnome-terminal-jasoncolors.sh
@@ -0,0 +1,6 @@
+# iterm2-jasoncolors.itermcolors
+#!/bin/bash
+gconftool-2 --set /apps/gnome-terminal/profiles/Default/foreground_color --type string "#45373C"
+gconftool-2 --set /apps/gnome-terminal/profiles/Default/background_color --type string "#D5CCBA"
+gconftool-2 --set /apps/gnome-terminal/profiles/Default/bold_color --type string "#45373C"
+gconftool-2 --set /apps/gnome-terminal/profiles/Default/palette --type string "#20111B:#BE100E:#858162:#EAA549:#426A79:#97522C:#989A9C:#968C83:#5E5252:#BE100E:#858162:#EAA549:#426A79:#97522C:#989A9C:#D5CCBA"
